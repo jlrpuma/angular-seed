4e0ccc7e9531c1148f930efdf25fd7170dec6062
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-27 21:14:53 -0500

    removing some unnecesary stuff

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -12,7 +12,7 @@ describe('my app', function() {
 
 
   describe('view1', function() {
-
+    /*
     beforeEach(function() {
       browser.get('index.html#!/view1');
     });
@@ -22,21 +22,6 @@ describe('my app', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
+    */
   });
 });
--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function(config) {
       'lib/angular/angular.js',
       'lib/angular-route/angular-route.js',
       '../node_modules/angular-mocks/angular-mocks.js',
-      'core/**/*.js',
       'view*/**/*.js'
     ],
 
